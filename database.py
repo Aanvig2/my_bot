ae0446eaf4e5dfd78af0a3669aa0e7eb9879315b
Aanvig2 <aanvigahoi@gmail.com>
2025-07-30 15:30:46 +0530

    update

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
 from datetime import datetime, timedelta
+import os
 
+os.makedirs("/data", exist_ok=True)
 conn = sqlite3.connect("/data/bot.db", check_same_thread=False)
 cursor = conn.cursor()
 
